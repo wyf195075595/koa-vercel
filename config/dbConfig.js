b034ae9ae97173059c0c4d626e522f4706c1c612
wyf195075595 <1950755953@qq.com>
2023-06-06 15:58:42 +0800

    增加部署环境变量

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,13 +2,13 @@
  * @Description: 
  * @Author:  
  * @Date: 2023-06-06 11:08:25
- * @LastEditTime: 2023-06-06 15:37:43
+ * @LastEditTime: 2023-06-06 15:58:22
  * @LastEditors:  
  */
 const database = {
     HOST: 'mysql.sqlpub.com', //数据库地址
     USER: 'sysadmin',
-    PASSWORD: '',
+    PASSWORD: process.env.MYSQL_PWD,
     DATABASE: 'mydb1024'    //数据库名称
   }
   module.exports = { database }
