e209244f21b3ac6f5c0fe6577b27a79817d3ccce
wyf195075595 <1950755953@qq.com>
2023-06-19 14:19:44 +0800

    资源引用设置

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,13 +2,13 @@
  * @Description: 
  * @Author:  
  * @Date: 2023-06-06 11:08:25
- * @LastEditTime: 2023-06-12 15:54:50
+ * @LastEditTime: 2023-06-19 12:01:27
  * @LastEditors:  
  */
 const database = {
     HOST: 'mysql.sqlpub.com', //数据库地址
     USER: 'sysadmin',
-    PASSWORD: "500524a996b360a9",//process.env.MYSQL_PWD
+    PASSWORD: process.env.MYSQL_PWD,//process.env.MYSQL_PWD
     DATABASE: 'mydb1024'    //数据库名称
   }
   module.exports = { database }
